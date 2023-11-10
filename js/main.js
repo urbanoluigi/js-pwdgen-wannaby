385869b711980dc83c18025db75004010b07e1c3
urbanoluigi <urrbanoluigi@gmail.com>
2023-11-10 09:59:22 +0100

    maybe finish but i don't why doesn't work

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
-const lunghezzapassword = document.getElementById("lunghezza")
-const caratteriminuscoli = document.getElementById("caratteri-minuscoli")
-const caratterimaiuscoli = document.getElementById("caratteri-numerici")
-const caratterispeciali = document.getElementById("caratteri-speciali")
+const lunghezzaPassword = document.getElementById("lunghezza")
+const caratteriMinuscoli = document.getElementById("caratteri-minuscoli")
+const caratteriMaiuscoli = document.getElementById("caratteri-maiuscoli")
+const caratteriNumerici = document.getElementById("caratteri-numerici")
+const caratteriSpeciali = document.getElementById("caratteri-speciali")
 const generaBtn = document.getElementById("genera-btn")
 const risultato = document.getElementById("risultato")
 
-const listacaratteriminuscoli = "abcdefghijklmnopqwrstuvwxyz"
-const listacaratterimaiuscoli = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const listacaratterinumeri = "0123456789"
-const listacaratterispeciali = "!'£$%&/()=?^*#{}[]"
+const listacaratteriMinuscoli = "abcdefghijklmnopqwrstuvwxyz"
+const listacaratteriMaiuscoli = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const listacaratteriNumerici = "0123456789"
+const listacaratteriSpeciali = "!'£$%&/()=?^*#{}[]"
 
 generaBtn.addEventListener("click", generaPassword)
 
 function generaPassword(){
     let stringainiziale = ""
-let risultato = ""
-if(caratteriminuscoli.checked) stringainiziale += listacaratteriminuscoli
-if(caratterimaiuscoli.checked) stringainiziale += listacaratterimaiuscoli
-if(caratterinumerici.checked) stringainiziale += listacaratterinumeri
-if(caratterispeciali.checked) stringainiziale += listacaratterispeciali
+    let risultato = ""
+    if(caratteriminuscoli.checked) stringainiziale += listacaratteriMinuscoli
+    if(caratterimaiuscoli.checked) stringainiziale += listacaratteriMaiuscoli
+    if(caratterinumerici.checked) stringainiziale += listacaratteriNumerici
+    if(caratterispeciali.checked) stringainiziale += listacaratteriSpeciali
 
-for(let i =0; i<lunghezzapassword.ariaValueMax; i++){
-    risultato += stringainiziale.chartAt(
+    for(let i =0; i<lunghezzapassword.ariaValueMax; i++){
+      risultato += stringainiziale.chartAt(
         Math.floor(Math.random()*stringainiziale.length)
     )
 }
